b842b5a2cfef47a08b9e57b8dac6451a9cf3f81a
TFlexSoom <tchilbert@live.com>
2020-05-23 12:54:20 -0700

    Added Documentation

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+ * Tristan Hilbert
+ * 5/23/2020
+ * Entry File For Package
+ * 
+ */
+
+/*
+ * The following sections refer to different directories of source code
+ * within the 'src/' directory. Please Refer to "pipeline" in the documentation
+ * 
+ */
+
+// CLI
+// We need:
+// -- Input folder
+// -- Output folder
+// -- Default Template
+// Other Options TBD
+
+
+
+
+
+// Parsing and REGEX of Folder
+
+
+// Grunt Tasks
+
+
+// Finalization and Project Structure
+
